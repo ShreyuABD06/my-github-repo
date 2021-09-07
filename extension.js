810336ff3a16242f6d5584146a6ec3eb06be9347
ShreyuABD06 <shreyasc.997@gmail.com>
2021-09-07 15:30:05 +0530

    Version 2 Search All Users

--- extension.js
+++ extension.js
@@ -4,25 +4,38 @@ const axios = require('axios');
  * @param {vscode.ExtensionContext} context
  */
 
-async function activate(context) {
-	const URL='https://api.github.com/users/ShreyuABD06/repos';
-	const res = await axios.get(URL);	
-	const projects = res.data.map(project => {
+async function activate(context) {	
+	let disposable = vscode.commands.registerCommand('my-github-repo.searchMyProjects', 
+	async function () {	
+		const user = await vscode.window.showInputBox({
+			placeHolder:'search user',
+			prompt: 'User Name?'
+		})	
+		console.log(user);
+		if (user==null || user==='' || user===undefined) {
+			await vscode.window.showInformationMessage('Please select valid user!');
+			return;
+		}
+		const URL=`https://api.github.com/users/${user}/repos`;
+		const res = await axios.get(URL);	
+		const projects = res.data.map(project => {
 		return {
 		label: project.name,
 		detail: project.full_name,	
 		link: project.html_url,	
-	}});
-
-	console.log(projects);	
-	let disposable = vscode.commands.registerCommand('my-github-repo.searchMyProjects', 
-	async function () {		
+		}});
+		console.log(projects.length);
+		if(projects==null || projects.length<1 || projects===undefined){
+			await vscode.window.showInformationMessage('User has 0 repositories or user doesnt exist. Please search for different user!');
+			return;
+		}
 		const project = await vscode.window.showQuickPick(projects,{
 			matchOnDetail: true
 		}
 		)
-		if(project==null)return;
-			
+		if(project==null){
+			return;
+		}			
 		vscode.env.openExternal(project.link);
 	});
 	context.subscriptions.push(disposable);
